d051aa7eb215bcbaa57e76e3c23bbe3f71032fcc
RastaRoot <57710888+RastaRoot@users.noreply.github.com>
2024-03-10 10:52:53 +0700

    Add files via upload

---
+++ Lesson9/SearchForElement.java
@@ -0,0 +1,18 @@
+package Lesson9;
+
+public class SearchForElement {
+    public static int getArrayElement(int[] array, int number) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == number) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {9, 8, 7, 6, 5};
+        System.out.println(getArrayElement(array, 7));
+    }
+}
