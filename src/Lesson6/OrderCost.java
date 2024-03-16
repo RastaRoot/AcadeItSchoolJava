dac5778e4fc2e85204846f70068c63be7bd36385
RastaRoot <novostinew@mail.ru>
2024-03-16 19:48:31 +0700

    Update OrderCost.java

--- src/Lesson6/OrderCost.java
+++ src/Lesson6/OrderCost.java
@@ -3,23 +3,23 @@ package Lesson6;
 public class OrderCost {
     public static double getOrderCost(int product1Amount, double product1Price,
                                       int product2Amount, double product2Price) {
-        double totalPrice = product1Amount * product1Price + product2Amount * product2Price;
+        double fullCost = product1Amount * product1Price + product2Amount * product2Price;
         int productsAmount = product1Amount + product2Amount;
 
-        final double discountRate1 = 0.9;
-        final double discountRate2 = 0.95;
+        final double maxDiscountRate = 0.9;
+        final double partialDiscountRate = 0.95;
 
         final int discountQuantityThreshold = 10;
         final int discountPriceThreshold = 1000;
 
         double finalCost;
 
-        if (productsAmount >= discountQuantityThreshold && totalPrice >= discountPriceThreshold) {
-            finalCost = totalPrice * discountRate1;
-        } else if (productsAmount >= discountQuantityThreshold || totalPrice >= discountPriceThreshold) {
-            finalCost = totalPrice * discountRate2;
+        if (productsAmount >= discountQuantityThreshold && fullCost >= discountPriceThreshold) {
+            finalCost = fullCost * maxDiscountRate;
+        } else if (productsAmount >= discountQuantityThreshold || fullCost >= discountPriceThreshold) {
+            finalCost = fullCost * partialDiscountRate;
         } else {
-            finalCost = totalPrice;
+            finalCost = fullCost;
         }
 
         return finalCost;
