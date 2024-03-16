8be656f2a253a20b83844c3d30b6af7fcd8ef2a7
RastaRoot <novostinew@mail.ru>
2024-03-16 23:00:38 +0700

    Update MultiplicationTable.java

--- src/Courseworks/MultiplicationTable.java
+++ src/Courseworks/MultiplicationTable.java
@@ -6,26 +6,26 @@ public class MultiplicationTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("До какого числа необходимо сформировать таблицу?");
+        System.out.print("Введите последнее число таблицы умножения: ");
         int lastNumber = scanner.nextInt();
 
-        System.out.print("    |");
+        System.out.print("     |");
 
         for (int i = 1; i <= lastNumber; i++) {
-            System.out.printf("%3d ", i);
+            System.out.printf("%4d ", i);
         }
 
         System.out.println();
-        System.out.print("----|");
+        System.out.print("-----|");
 
-        String indentLine = "----".repeat(lastNumber);
+        String indentLine = "-----".repeat(lastNumber);
         System.out.println(indentLine);
 
         for (int i = 1; i <= lastNumber; i++) {
-            System.out.printf("%3d |", i);
+            System.out.printf("%4d |", i);
 
             for (int j = 1; j <= lastNumber; j++) {
-                System.out.printf("%3d ", i * j);
+                System.out.printf("%4d ", i * j);
             }
 
             System.out.println();
